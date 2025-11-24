7f0beb3dfcd1cb466e83c7bc8bb75bff5817c4c5
Leesomoney <127239280+sidemix@users.noreply.github.com>
2025-11-24 14:25:41 -0500

    1

--- main.py
+++ main.py
@@ -1170,3 +1170,4 @@ async def clear_events(db: SessionLocal = Depends(get_db)):
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
+    
