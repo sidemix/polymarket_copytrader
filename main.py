a9da48369d9f6891aeb5f61f8d80230d8edfaf5e
Leesomoney <127239280+sidemix@users.noreply.github.com>
2025-11-24 12:43:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -467,3 +467,4 @@ async def get_events(db: SessionLocal = Depends(get_db)):
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
+    
